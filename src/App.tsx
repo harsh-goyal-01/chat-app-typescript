7d5aafcd7ef43fbc0f84a256d83fa8a3e3617af9
harsh-goyal-01 <77243780+harsh-goyal-01@users.noreply.github.com>
2023-06-10 12:34:53 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Textbox from './components/Textbox/Textbox';
+import * as antd from 'antd';
+const App: React.FC = () => {
+  return (
+    <div>
+      <Textbox /> 
+    </div>
+  );
+};
+
+export default App;
